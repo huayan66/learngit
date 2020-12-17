8be2de695561c58f1993124285dbb12832daccea
chenhuayan <2414022154@qq.com>
2020-12-17 21:54:19 +0800

    提交hello.java

---
+++ src/hello.java
@@ -0,0 +1,9 @@
+public class Hello { //类名是Hello
+    public static void main(String[] arg) { //方法命是main
+        //向屏幕输入文本:
+        System.out.print("hello world!");
+		/*多行注释开始
+		 注释内容
+		 注释结束*/
+    } //方法定义结束
+} //Hello定义结束
